bdfce1b9d84b82a3a4a1c3607e319c8472b35a05
MarcinBamburski1987 <marcin2511@op.pl>
2020-07-28 11:47:28 +0200

    this commit fix problem from prev commit. In method ngOnInit is subsription from pipe async and is observable - udemy 181 movie 5min 40 sek

--- src/app/pages/categories/movies-in-category/movies-in-category.component.ts
+++ src/app/pages/categories/movies-in-category/movies-in-category.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { HttpService } from '../../../services/http.service';
 import { Movie } from '../../../models/movie';
@@ -16,7 +16,16 @@ export class MoviesInCategoryComponent implements OnInit {
   constructor(private http: HttpService, private route: ActivatedRoute) {}
 
   ngOnInit() {
-     const category = this.route.snapshot.paramMap.get('category');
-     this.movies = this.http.getMoviesFromCategory(category);
+    //ten kod powoduje pewien problem ze ścieżką routingu, gdy przekazuje się przez param
+     // const category = this.route.snapshot.paramMap.get('category');
+     // this.movies = this.http.getMoviesFromCategory(category);
+
+    //ten kod to naprawia, tutaj musi być subskrypcja dodana przez pipe async - observable
+    this.movies = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+      this.http.getMoviesFromCategory(params.get('category'))
+      )
+    );
+
   }
 }
