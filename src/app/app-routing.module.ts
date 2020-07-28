8690ea86b8779e3d2f94b68ae8f35c8155f95bb3
MarcinBamburski1987 <marcin2511@op.pl>
2020-07-28 11:24:37 +0200

    save - nie chciało mi się już opisywać

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MoviesComponent } from './pages/movies/movies.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { MovieDetailsComponent } from './pages/movies/movie-details/movie-details.component';
+import { MoviesInCategoryComponent } from './pages/categories/movies-in-category/movies-in-category.component';
 
 const routes: Routes = [
-  {path: 'movies', component: MoviesComponent}
-]
+  { path: '', redirectTo: '/movies', pathMatch: 'full' },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'movie/:id', component: MovieDetailsComponent },
+  { path: 'categories', component: CategoriesComponent },
+  {path: 'category/:category', component: MoviesInCategoryComponent},
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
